3d1abd29a114612c74ee3b104f64d60e415ac4e5
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-03-12 23:14:18 +0500

    Navbar Me Kam KYA

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./app.css";
 
-import logo from "../../1.png"
+// import logo from "../../1.png"
 
 function Navbar() {
   return (
     <nav>
-      
+      <nav className="navbar bg-body-tertiary p-2">
+        <div className="container containerOfNavbar p-0 d-flex align-items-center">
+          <div>
+            <img src="../../1.png" alt="Bootstrap" width="60" height="60" />
+          </div>
+          <ul className='ulOfNavbar'>
+            <li>HOME</li>
+            <li>SERVICE</li>
+            <li>PORTFOLIO</li>
+            <li>SKILL</li>
+            <li>TESTIMONIAL</li>
+            <li>BLOG</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+      </nav>
     </nav>
   )
 }
