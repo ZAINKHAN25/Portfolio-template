263ceb9bcba78224f6f2b3f0971b74ff3cf78bab
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-03-13 22:51:22 +0500

    Kam Chal rha hai

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import "./app.css";
 
-// import logo from "../../1.png"
-
 function Navbar() {
   return (
-    <nav>
-      <nav className="navbar navbarDiv p-2">
-        <div className="container containerOfNavbar p-0 d-flex align-items-center">
-          <div>
-            <img src="../../1.png" style={{ borderRadius: "50%", cursor: "pointer"}} alt="Bootstrap" width="60" height="60" />
-          </div>
-          <ul className='ulOfNavbar'>
-            <li>HOME</li>
-            <li>SERVICE</li>
-            <li>PORTFOLIO</li>
-            <li>SKILL</li>
-            <li>TESTIMONIAL</li>
-            <li>BLOG</li>
-            <li>CONTACT</li>
-          </ul>
+    <nav className="navbar navbarDiv p-2 ">
+      <div className="container containerOfNavbar p-0 d-flex align-items-center">
+        <div>
+          <img src="../../1.png" style={{ borderRadius: "50%", cursor: "pointer" }} alt="Bootstrap" width="60" height="60" />
         </div>
-      </nav>
+        <ul className='ulOfNavbar'>
+          <li data-to-scrollspy-id="first">HOME</li>
+          <li data-to-scrollspy-id="second">SERVICE</li>
+          <li>PORTFOLIO</li>
+          <li>SKILL</li>
+          <li>TESTIMONIAL</li>
+          <li>BLOG</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
     </nav>
   )
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from "../../Components/Navbar/index.jsx";
+import HomeComponent from "../../Components/HomeComponent/index.jsx";
 import "./Home.css"
 
 function Home() {
   return (
     <div className="body">
       <Navbar />
+      <HomeComponent/>
     </div>
   )
 }
